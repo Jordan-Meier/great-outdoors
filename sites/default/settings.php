1c42ad6f065518e582809d67e992d54f04bc5212
jsonberry <jason.s.awbrey@gmail.com>
2016-05-17 15:00:04 -0700

    Add tao theme

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -212,7 +212,21 @@
  *   );
  * @endcode
  */
-$databases = array();
+$databases = array (
+  'default' => 
+  array (
+    'default' => 
+    array (
+      'database' => 'pantheon',
+      'username' => 'pantheon',
+      'password' => '4da0e65abe8f41fd98f512a84d4b731b',
+      'host' => '127.0.0.1',
+      'port' => '8889',
+      'driver' => 'mysql',
+      'prefix' => '',
+    ),
+  ),
+);
 
 /**
  * Access control for update.php script.
@@ -244,7 +258,7 @@ $update_free_access = FALSE;
  *   $drupal_hash_salt = file_get_contents('/home/example/salt.txt');
  *
  */
-$drupal_hash_salt = '';
+$drupal_hash_salt = 'lcEJCd8P3LAvG_WBmNaAcyJQpxikXXpCvNrvUIlubBk';
 
 /**
  * Base URL (optional).
